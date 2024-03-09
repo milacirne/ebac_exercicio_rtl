8b4f6af469ff7e678454b4fd895eaf9a0a2de0ad
Camila Cirne <camila.cirne22@gmail.com>
2024-03-09 02:36:12 -0300

    realizando testes de integração

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,30 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve inserir dois comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Primeiro comentário do teste',
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-submit-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário do teste',
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('comment-submit-button'));
+
+        const commentElements = screen.getAllByTestId('comment-item');
+        expect(commentElements).toHaveLength(2)
+        expect(commentElements[0]).toHaveTextContent('Primeiro comentário do teste');
+        expect(commentElements[1]).toHaveTextContent('Segundo comentário do teste');
+    });
 });
+
